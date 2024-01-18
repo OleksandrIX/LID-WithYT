a88acca2dcbd8a8c7e88490a709a3558fdffaabf
OleksandrIX <zagorenkosasha@gmail.com>
2024-01-18 11:42:08 +0200

    init project

---
+++ tensorflow/compile_model.py
@@ -0,0 +1,15 @@
+import tensorflow
+from yaml import load, FullLoader
+from loguru import logger
+
+
+def compile_model(model):
+    path = "/home/oleksandr/Projects/PythonProject/NatoHackathon/DownloadVideoWithYoutube/tensorflow/config.yml"
+    config = load(open(path, "rb"), Loader=FullLoader)
+
+    optimizer = tensorflow.keras.optimizers.Adam(learning_rate=config["learning_rate"])
+    model.compile(optimizer=optimizer,
+                  loss="categorical_crossentropy",
+                  metrics=["accuracy"])
+    logger.info("Model compiled.")
+    return model
